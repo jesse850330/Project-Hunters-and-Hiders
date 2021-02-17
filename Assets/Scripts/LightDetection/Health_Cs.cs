81d10ea86295cff7a6940df69b3f0795b9fcd239
lorne <lawnhawke@gmail.com>
2021-02-17 23:09:55 +1300

    Push

--- Assets/Scripts/LightDetection/Health_Cs.cs
+++ Assets/Scripts/LightDetection/Health_Cs.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Health_Cs : MonoBehaviour
 {
     public float Health = 50;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Playerview;
+    public GameObject LightDetection;   
 
     // Update is called once per frame
     void Update()
     {
         
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Flashlight")
+        {
+            LightDetection.SetActive(true);
+            Playerview.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Flashlight")
+        {
+            LightDetection.SetActive(true);
+            Playerview.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Flashlight")
+        {
+            LightDetection.SetActive(false);
+            Playerview.SetActive(true);
+        }
     }
 }
